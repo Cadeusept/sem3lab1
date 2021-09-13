cc8ede3f02f6f9828e003eceef35e905b25400f2
Cadeusept <matwey.likhota@gmail.com>
2021-09-13 13:28:49 +0300

    student struct+methods

--- include/student.hpp
+++ include/student.hpp
@@ -3,4 +3,11 @@
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
 
+struct Student{
+    std::string name;
+    std::any group;
+    std::any avg;
+    std::any debt;
+}
+
 #endif // INCLUDE_HEADER_HPP_
