8ab2deee91211b6f3c7b433d5f10a2960d4dca65
Cadeusept <matwey.likhota@gmail.com>
2021-09-17 16:21:15 +0300

    works+tips

--- demo/main.cpp
+++ demo/main.cpp
@@ -3,27 +3,6 @@
 #include <fstream>
 #include <iostream>
 
-int main() {
-    std::string jsonPath = "~/Рабочий стол/students.json";
-
-
-    std::ifstream file{jsonPath};
-    if (!file) {
-        throw std::runtime_error{"unable to open json: " + jsonPath};
-    }
-
-    json data;
-    file >> data;
-
-    std::vector<student_t> students;
-    for (auto const& item : data.at("items")) {
-        auto student = item.get<student_t>()
-        students.push_back(student);
-    }
-    //нужно проверки сделатб
-    print(students, std::cout);
-}
-
 void print(const student_t& student, std::ostream& os) {
     //сделать для остальных атрибутов student проверку
     if (student.debt.type() == typeid(std::nullptr_t)) {
@@ -43,6 +22,27 @@ void print(const std::vector<student_t>& students, std::ostream& os) {
     for (auto const& student : students) {
         print(student, os);
     }
+}
+
+int main() {
+    std::string jsonPath = "~/Рабочий стол/students.json";
+
+
+    std::ifstream file{jsonPath};
+    if (!file) {
+        throw std::runtime_error{"unable to open json: " + jsonPath};
+    }
+
+    json data;
+    file >> data;
+
+    std::vector<student_t> students;
+    for (auto const& item : data.at("items")) {
+        auto student = item.get<student_t>()
+        students.push_back(student);
+    }
+    //нужно проверки сделатб
+    print(students, std::cout);
 }
 
 
